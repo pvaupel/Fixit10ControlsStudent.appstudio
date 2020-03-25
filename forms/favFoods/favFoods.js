51552a9cbb4553ac9b768858952ddb549ca1b0e1
pvaupel <pov46864@creighton.edu>
2020-03-25 13:12:15 -0500

    favFoods

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,23 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+btnDessertVoting.onclick=function(){
+  ChangeForm(dessertVoting)
+}
+
+
+
+favFoods.onshow=function(){
+  lstFoods.clear()
+  
+  for (i=0; i <= goodFoods.length -1; i ++) 
+        lstFoods.addItem(goodFoods[i])
+}
+
+lstFoods.onclick=function(choice){
+  if (typeof(choice) == "object") {
+        return
+      } else { 
+        let textChoice = NSB.$("lstFoods_" + choice).textContent
+        lblFoodMessage.value = `You picked ${textChoice}- that is a great choice!`
+  }
+}
