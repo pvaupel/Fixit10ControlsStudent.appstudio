4c5de995410637925a456f65ba5b594c13ba9654
pvaupel <pov46864@creighton.edu>
2020-03-25 13:28:20 -0500

    dessertVoting

--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -1,3 +1,26 @@
+//shortcut to favFoods
+btnFavFoods.onclick=function(){
+  ChangeForm(favFoods)
+}
 
+req1 = ""
 
-
+btnLogin.onclick=function(){
+  req1 = Ajax("http://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inpNetId.value + "&j_password=" + inpPassword.value)
+  
+  if (req1.status == 200) {
+        lblResponse.hidden = false
+        lblResponse.value = "Your LDAP return code was " + req1.responseText
+      } else {
+        lblResponse.hidden = false
+        lblResponse.value = "Error: " + req1.status;
+  }
+  
+  if (req1.responseText ==1) {
+        ChangeForm(favFoods)
+      } else {
+        inpNetId.value = ""
+        inpPassword.value = ""
+        lblResponse.value = `These login credentials were not valid. Please try again!`
+  }
+}
