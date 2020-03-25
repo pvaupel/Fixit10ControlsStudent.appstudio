7f20eea946ded9e9a6e817068ace986b02c849ac
pvaupel <pov46864@creighton.edu>
2020-03-25 15:09:24 -0500

    favExercises

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,17 @@
 
+btnFaveExercises.onclick=function(){
+  ChangeForm(ABfavExercises)
+}
 
 
-
-
+rdbDescriptions.onchange=function(){
+  if (rdbDescriptions.value == 0) {
+        lblResult.value = `I would agree that you are an 'optimistic' person too!`
+      } else if (rdbDescriptions.value == 1) { 
+        lblResult.value = `I would agree that you are a 'pessimistic' person too!`
+      } else if (rdbDescriptions.value == 2) { 
+        lblResult.value = `I would agree that you are a 'trusting' person too!`
+      } else if (rdbDescriptions.value == 3) { 
+        lblResult.value = `I would agree that you are an 'envious' person too!`
+      } 
+}
